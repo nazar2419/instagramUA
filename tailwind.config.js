0238eaca501ae275ce8cba96d27b6e593846860e
nazar2419 <nazarx095@gmail.com>
2022-04-03 21:45:54 +0300

    page login

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,24 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      while: '#ffffff',
+      blue: {
+        medium: '#005c98'
+      },
+      black: {
+        light: '#005c98',
+        faded: '#00000059'
+      },
+      gray: {
+        base: '#616161',
+        background: '#fafafa',
+        primary: '#dbdbdb'
+      },
+      red: {
+        primary: '#ed4956'
+      }
+    }
   },
   plugins: [],
 }
