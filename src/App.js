4ed5a28b9dad0014bbf41dbd3884569819fa3c35
nazar2419 <nazarx095@gmail.com>
2022-04-12 15:02:41 +0300

    component header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
+import useAuthListener from './hooks/use-auth-listener';
+import UserContext from './context/user';
+
 const Login = lazy(() => import ('./pages/login'));
 const SignUp = lazy(() => import ('./pages/signup'));
+const Dashboard = lazy(() => import('./pages/dashboard'));
 const NotFound = lazy(() => import('./pages/notfound'));
 
-function App() {
+export default function App() {
+  const { user } = useAuthListener();
+
   return (
-   <Router>
-     <Suspense fallback={<p>Loading...</p>}>
-      <Routes>
-       
-        <Route path={ROUTES.LOGIN} element={<Login/>} />
-        <Route path={ROUTES.SIGN_UP} element={<SignUp/>} />
-        <Route path="*" element={<NotFound/>} />
-      </Routes>
-     </Suspense>
-   </Router>
+    <UserContext.Provider value={{user}}>
+      <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes>
+          <Route path={ROUTES.LOGIN} element={<Login/>} />
+          <Route path={ROUTES.SIGN_UP} element={<SignUp/>} />
+          <Route path={ROUTES.DASHBOARD} element={<Dashboard/>} />
+          <Route path="*" element={<NotFound/>} />
+        </Routes>
+      </Suspense>
+      </Router>
+   </UserContext.Provider>
   );
 }
-
-export default App;
--- src/seed.js
+++ src/seed.js
@@ -1,7 +1,7 @@
 export function seedDatabase(firebase) {
   const users = [
     {
-      userId: 'Vsc3d0ZuSrcn2NK4kQIjD8MXplP2',
+      userId: '8kj36FrhgtOFHOn512UqGSjCLOv2',
       username: 'nazar',
       fullName: 'Nazar Pihur',
       emailAddress: 'nazar@gmail.com',
@@ -15,7 +15,7 @@ export function seedDatabase(firebase) {
       fullName: 'Raffaello Sanzio da Urbino',
       emailAddress: 'raphael@sanzio.com',
       following: [],
-      followers: ['Vsc3d0ZuSrcn2NK4kQIjD8MXplP2'],
+      followers: ['8kj36FrhgtOFHOn512UqGSjCLOv2'],
       dateCreated: Date.now()
     },
     {
@@ -24,7 +24,7 @@ export function seedDatabase(firebase) {
       fullName: 'Salvador Dalí',
       emailAddress: 'salvador@dali.com',
       following: [],
-      followers: ['Vsc3d0ZuSrcn2NK4kQIjD8MXplP2'],
+      followers: ['8kj36FrhgtOFHOn512UqGSjCLOv2'],
       dateCreated: Date.now()
     },
     {
@@ -33,7 +33,7 @@ export function seedDatabase(firebase) {
       fullName: 'George Orwell',
       emailAddress: 'george@orwell.com',
       following: [],
-      followers: ['Vsc3d0ZuSrcn2NK4kQIjD8MXplP2'],
+      followers: ['8kj36FrhgtOFHOn512UqGSjCLOv2'],
       dateCreated: Date.now()
     }
   ];
